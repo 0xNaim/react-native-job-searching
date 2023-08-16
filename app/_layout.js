df150b1251e77b9dcda8f164eb2da35fc3093148
Omar Faruq <naimislam3545@gmail.com>
2023-08-16 18:48:34 +0600

    feat: popular job page added

---
+++ app/_layout.js
@@ -0,0 +1,24 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+
+    if (!fontsLoaded) return null;
+  }, [fontsLoaded]);
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
+
+export default Layout;
